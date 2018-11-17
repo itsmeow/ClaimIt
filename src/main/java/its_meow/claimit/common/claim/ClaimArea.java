88336a38f2b1ec279f8c6a24a7a9d9ad8dce0e54
BuildTools <mods.itsmeow@gmail.com>
2018-11-17 13:32:46 -0500

    Add more accessor methods and corner methods

--- src/main/java/its_meow/claimit/common/claim/ClaimArea.java
+++ src/main/java/its_meow/claimit/common/claim/ClaimArea.java
@@ -4,47 +4,84 @@ import java.util.UUID;
 
 import javax.annotation.Nullable;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 
 public class ClaimArea {
 
 	private int posX;
-	private int posY;
+	private int posZ;
 	private final int dimID;
 	private int sideLengthX;
-	private int sideLengthY;
+	private int sideLengthZ;
 	private UUID ownerUUID;
 
-	public ClaimArea(int dimID, int posX, int posY, int sideLengthX, int sideLengthY, String ownerName) {
+	public ClaimArea(int dimID, int posX, int posZ, int sideLengthX, int sideLengthZ, String ownerName) {
 		this.dimID = dimID;
 		this.posX = posX;
-		this.posY = posY;
+		this.posZ = posZ;
 		this.sideLengthX = sideLengthX;
-		this.sideLengthY = sideLengthY;
+		this.sideLengthZ = sideLengthZ;
 		this.ownerUUID = EntityPlayer.getOfflineUUID(ownerName);
+		// Simplify main corner to the lowest x and y value
+		if(this.sideLengthX < 0 || this.sideLengthZ < 0) {
+			if(this.sideLengthX < 0) {
+				this.posX += this.sideLengthX;
+				this.sideLengthX = Math.abs(this.sideLengthX);
+			}
+			if(this.sideLengthZ < 0) {
+				this.posZ += this.sideLengthZ;
+				this.sideLengthZ = Math.abs(this.sideLengthZ);
+			}
+		}
 	}
 
-	public ClaimArea(int dimID, int posX, int posY, int sideLengthX, int sideLengthY, EntityPlayer player) {
-		this(dimID, posX, posY, sideLengthX, sideLengthY, player.getName());
+	public ClaimArea(int dimID, int posX, int posZ, int sideLengthX, int sideLengthZ, EntityPlayer player) {
+		this(dimID, posX, posZ, sideLengthX, sideLengthZ, player.getName());
 	}
 	
-	/** Returns posX and posY as BlockPos **/
+	/** Returns posX and posZ as BlockPos **/
 	public BlockPos getMainPosition() {
-		return new BlockPos(posX, 0, posY);
+		return new BlockPos(posX, 0, posZ);
 	}
 	
+	/** Returns the highest X and Z corner **/
+	public BlockPos getHXZPosition() {
+		return new BlockPos(posX + sideLengthX, 0, posZ + sideLengthZ);
+	}
 	
+	/** Returns the highest X and lowest Z corner **/
+	public BlockPos getHXLZPosition() {
+		return new BlockPos(posX + sideLengthX, 0, posZ);
+	}
 	
-	@Nullable
-	public BlockPos[] getTwoClaimCorners() {
-		if(this.sideLengthX == 0 || this.sideLengthY == 0) {
-			return null;
-		}
+	/** Returns the lowest X and the highest Z corner **/
+	public BlockPos getLXHZPosition() {
+		return new BlockPos(posX, 0, posZ + sideLengthZ);
+	}
+	
+	public BlockPos[] getTwoMainClaimCorners() {
 		BlockPos[] corners = new BlockPos[2];
 		corners[0] = this.getMainPosition();
-		
+		corners[1] = this.getHXZPosition();
 		return corners;
+	}
+	
+	@Nullable
+	public UUID getOwner() {
+		return this.ownerUUID;
+	}
+	
+	public int getDimensionID() {
+		return dimID;
+	}
+	
+	public World getWorld() {
+		return DimensionManager.getWorld(this.dimID);
 	}
 
 }
