a22429beb5f82305ca4d4a4b0d86a1d938385112
itsmeow <mods.itsmeow@gmail.com>
2019-05-04 19:01:34 -0400

    Add version replace to main class

--- src/main/java/its_meow/claimit/ClaimIt.java
+++ src/main/java/its_meow/claimit/ClaimIt.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class ClaimIt {
     
     public static final String MOD_ID = "claimit";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "@VERSION@";
     public static final String NAME = "ClaimIt";
     public static final String acceptedMCV = ClaimItAPI.acceptedMCV;
     public static final String updateJSON = ClaimItAPI.updateJSON;
